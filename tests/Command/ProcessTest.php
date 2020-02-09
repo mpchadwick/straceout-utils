715dca134e650c68c6923a059dbae9988b5e3459
Max Chadwick <mpchadwick@gmail.com>
2020-02-09 17:29:33 -0500

    Add test for Process command

---
+++ tests/Command/ProcessTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use StraceOutUtils\Command;
+use PHPUnit\Framework\TestCase;
+
+class ProcessTest extends TestCase
+{
+    public function testExecute()
+    {
+        $application = new Application;
+        $application->add(new Command\Process());
+        $command = $application->find('process');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'file' => 'tests/fixtures/brk-before.txt'
+        ]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertEquals(
+            $output,
+            file_get_contents('tests/fixtures/brk-after.txt')
+        );
+    }
+}
